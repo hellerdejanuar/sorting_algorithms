0e4bdbe84a14aed4cb0f4cea53ba4abd73b1eee2
hellerdejanuar <hellerdejanuar@gmail.com>
2022-06-08 12:18:32 -0700

    0 added

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, aux = 0, completed = 0;
+
+	while (!completed)
+	{
+		i = 0;
+		completed = 1;
+		
+		while (i + 1 < size)
+		{
+			if (array[i] > array[i + 1])
+			{
+				completed = 0;
+
+				aux = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = aux;
+
+				print_array(array, size);
+
+				i++;
+			}
+
+			else
+				i++;
+		}
+	}
+}
