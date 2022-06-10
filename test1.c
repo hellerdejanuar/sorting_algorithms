b9cbc1a81598b7b060f588733212fb94508fe33d
hellerdejanuar <hellerdejanuar@gmail.com>
2022-06-09 17:57:26 -0700

    1 add

--- test1.c
+++ test1.c
@@ -17,23 +17,39 @@ void insertion_sort_list(listint_t **list)
 
 		while (curr_node->n < cmp_node->n)
 		{
+			/* prev node or head */
 			if (cmp_node->prev != NULL)
+			{
 				cmp_node->prev->next = curr_node;
+				printf("asdf\n");
+			}
 			else
-				(*list) = curr_node;
+			{
+				list = &curr_node;
+				printf("swap list\n");
+			}
 
 			curr_node->prev = cmp_node->prev;
+
+			/* next node or NULL */
 			cmp_node->next = curr_node->next;
 			if (curr_node->next != NULL)
 				curr_node->next->prev = cmp_node;
+
+			/* swap nodes */
 			curr_node->next = cmp_node;
 			cmp_node->prev = curr_node;
 		
+			if (curr_node->prev == NULL)
+				list = &curr_node;
+	
 			print_list(*list);
 
 			if (curr_node->prev == NULL)
 				break;
-			cmp_node = curr_node->prev;
+			else
+				cmp_node = curr_node->prev;
+				/* keep swapping */
 		}
 
 		curr_node = last_ord_node;
